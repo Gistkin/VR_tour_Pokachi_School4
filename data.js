793a9dc711309dc9cda2f0ae7acb27bee84280ae
Gistkin <80309998+Gistkin@users.noreply.github.com>
2024-05-16 13:55:31 +0500

    Upload VR tour

---
+++ data.js
@@ -0,0 +1,287 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.12699633506305297,
+        "pitch": 0.32102506906274186,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1285569145104777,
+          "pitch": 0.5013308102009972,
+          "rotation": 4.71238898038469,
+          "target": "1-2"
+        },
+        {
+          "yaw": -0.7476630103936053,
+          "pitch": 0.32073249364727907,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7754423163277728,
+          "pitch": -0.007885123020262341,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Медиатека&nbsp;</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">учебный всенаправленный класс</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0515559536196601,
+          "pitch": 0.5814527886697469,
+          "rotation": 3.9269908169872414,
+          "target": "0-1"
+        },
+        {
+          "yaw": 2.284141233323309,
+          "pitch": 0.707988103750786,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": -0.6069053924647569,
+          "pitch": 0.07615773572487328,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1305724283609262,
+          "pitch": -0.011433359085703643,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">3D моделирование</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">разработка и печать&nbsp;</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.5407544650649339,
+        "pitch": 0.10353561667309563,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2061438546835035,
+          "pitch": 0.31803551250313156,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -0.5381644202964857,
+          "pitch": 0.048177074996532454,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0933512653406279,
+          "pitch": 0.05160545385713178,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Робототехника</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Конструирование и програмирование</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.930935808772805,
+          "pitch": 0.10705375835177655,
+          "rotation": 0,
+          "target": "4-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.03041488762923983,
+          "pitch": -0.04013651477274749,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Компьютерный класс</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">програмирование и разработка</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "4-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -1.6147216659714694,
+        "pitch": 0.2739370391292013,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5688309711350641,
+          "pitch": 0.47410898092102727,
+          "rotation": 12.566370614359176,
+          "target": "5-6"
+        },
+        {
+          "yaw": -2.555565098294192,
+          "pitch": 0.4243139269859917,
+          "rotation": 0.7853981633974483,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.167496028472234,
+          "pitch": 0.08695983238191118,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Класс технологии</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">моделирование из фанеры</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "5-6",
+      "name": "6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": 0.6455385806268445,
+        "pitch": 0.1030780844689545,
+        "fov": 1.2988261968502064
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.744583062816604,
+          "pitch": 0.23368617828664462,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3983879077834551,
+          "pitch": -0.0425886320116593,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Класс деревообработки</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">работа на чпу станках</font></font>"
+        }
+      ]
+    }
+  ],
+  "name": "Pokachi qtvr",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
